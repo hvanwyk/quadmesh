c7cb996b187644d3126c6f1572eacb14a3847401
Hans-Werner van Wyk <hanswernervanwyk@gmail.com>
2016-04-11 17:43:44 -0500

    First commit

---
+++ drifter/src/drifter/data/preprocess_data.py
@@ -0,0 +1,80 @@
+'''
+Created on Apr 7, 2016
+
+@author: hans-werner
+'''
+
+def build_buoy_lookup(data_files, lookup_file):
+    '''
+    Build a lookup table listing all buoys and where to locate information on
+    them in the datafiles.
+    
+    Inputs
+        
+        datafiles: str, list of filenames containing drifter information
+        
+    Output
+    
+        buoy_lookup_table.dat: file in drifter/data, to store lookup table
+            headers: ID    file_num    first_line    last_line
+    '''
+    # --------------------------------------------------------------------------
+    # Import Data
+    # --------------------------------------------------------------------------
+    current_id = 'None'
+    list_id = []
+    list_file_num = []
+    list_first_line = []
+    list_last_line = []
+    #
+    # Loop over data files
+    #
+    file_num = 0
+    for file_name in data_files:
+        #
+        # Open data file
+        # 
+        with open(file_name,"r") as fin:
+            print('Opening read file: %s' % file_name)
+            file_num += 1
+            line_count = 0
+            for line in fin:
+                line_count += 1
+                line = line.split()
+                if current_id != line[0]:
+                    #
+                    # New buoy!
+                    #
+                    if line_count > 1:
+                        #
+                        # Record previous buoy's data
+                        # 
+                        list_last_line.append(line_count - 1)
+                    #
+                    # Update data
+                    # 
+                    current_id = line[0]
+                    list_id.append(current_id)
+                    list_first_line.append(line_count)
+                    list_file_num.append(file_num)
+
+    # --------------------------------------------------------------------------
+    # Sort according to ID
+    # --------------------------------------------------------------------------
+    
+    #
+    # Open lookup file
+    #
+    """with open(lookup_file, "w") as fout:
+        print('Opening file: %s' % lookup_file)            
+        buoy_info_line = "%8s %d %-8d %-8d \n"
+                     % (current_id, file_num,
+                        first_line, last_line)
+                        fout.write(buoy_info_line)
+if __name__ == '__main__':
+    data_file = ['buoydata_1_5000.dat', \
+                 'buoydata_5001_10000.dat',\
+                 'buoydata_10001_jun15.dat']
+    lookup_file = "buoy_lookup_table.dat"
+    build_buoy_lookup(data_file, lookup_file)"""
+                
---
+++ drifter/src/drifter/grid/cell.py
@@ -0,0 +1,98 @@
+from drifter.grid.edge import Edge
+from drifter.grid.vertex import Vertex
+
+class Cell(object):
+    """
+    Description: (Tree of) Rectangular cell(s) in mesh
+    
+    
+    Attributes: 
+    
+        type - Cells can be one of the following types:
+            
+            ROOT   - cell on coarsest level
+            BRANCH - cell has parent(s) as well as children
+            LEAF   - cell on finest refinement level
+         
+        parent: cell of which current cell is a sub-cell
+         
+        children: list of sub-cells of current cell
+         
+        neighbors: addresses of neighboring cells
+         
+        depth: int, current refinement level 0,1,2,...
+         
+        rectangle: (x0, x1, y0, y1), where cell = [x0, x1]x[y0, y1]
+         
+        min_size: double, minimum possible mesh width
+     
+    
+    Methods: 
+     
+        refine, coarsen, has_children,  
+    """
+    # TODO: Is rectangle necessary? 
+    
+    NODE_NUM = 0
+    VERTEX_LIST = [] 
+    EDGE_LIST = []
+    
+    def __init__(self, parent, vertices, pos=None):
+        """
+        Description: Initializes the cell (sub-)tree
+        
+        Inputs: 
+                    
+            parent: parental Node
+            
+            rectangle: rectangle defining the cell boundary
+            
+            pos: own position in parent cell (NW, SW, NE, SE)
+        """
+ 
+        self.parent = parent
+        self.flag = False
+        self.position = pos
+        #
+        # Classify Node as ROOT or LEAF
+        # 
+        if self.parent == None:
+            self.type = 'ROOT'
+            self.children = []
+            self.depth = 0            
+        else:
+            self.type = 'LEAF'
+            self.children = {'NE': None, 'NW': None, 'SE': None, 'SW': None}
+            self.depth = parent.depth + 1
+
+            #
+            # Position vertices within Node
+            # 
+            v_sw, v_se, v_ne, v_nw = vertices
+            self.vertices = {'SW': v_sw, 'SE': v_se, 'NW': v_nw, 'NE': v_ne, 
+                             'N': None, 'S': None, 'E': None, 'W': None, 
+                             'M': None}
+           
+            #
+            # Define bounding rectangle
+            #
+            coordinates = [vertex.coordinates for vertex in vertices]
+            x0, y0 = min(coordinates)
+            x1, y1 = max(coordinates)
+            self.rectangle = (x0, x1, y0, y1)         
+                             
+            #
+            # Define edges
+            # 
+            e_we = Edge(v_sw, v_se, self)
+            e_sn = Edge(v_se, v_ne, self)
+            e_ew = Edge(v_ne, v_nw, self)
+            e_ns = Edge(v_nw, v_sw, self)
+            self.edges = {'S': e_we, 'E': e_sn, 'N': e_ew, 'W': e_ns}
+                 
+            if parent.type == 'ROOT':
+                self.position = None
+            else:
+                self.position = pos
+
+        
