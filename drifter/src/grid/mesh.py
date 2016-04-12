7a74019fc093c30d9039a1a8d7691bcfe154409a
Hans-Werner van Wyk <hanswernervanwyk@gmail.com>
2016-04-12 08:21:29 -0500

    trying to get rid of intermediate "drifter" folder

---
+++ drifter/src/grid/mesh.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 11, 2016
+
+@author: hans-werner
+'''
+
+class Mesh(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
