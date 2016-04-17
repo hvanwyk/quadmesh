0a93e12440e993ee8a4e17f15c221d7ec80e59f4
Hans-Werner van Wyk <hanswernervanwyk@gmail.com>
2016-04-17 09:15:10 -0500

    changed test names

---
+++ drifter/src/tests/test_grid.py
@@ -0,0 +1,23 @@
+import unittest
+
+class TestGrid(unittest.TestCase):
+    '''
+    Test grid module
+    '''
+      
+    def test_vertex(self):
+        pass
+    
+    def test_edge(self):
+        pass
+    
+    def test_cell(self):
+        pass
+    
+    def test_mesh(self):
+        pass
+    
+    
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
