dbf1534f05611732e9d2384d7d8c4a2e1fd38706
Hans-Werner van Wyk <hanswernervanwyk@gmail.com>
2017-02-08 12:41:18 -0600

    created plot adn gmrf modules

---
+++ src/gmrf.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 8, 2017
+
+@author: hans-werner
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
