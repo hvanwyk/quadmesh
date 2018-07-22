ae3f8c17bb866c54e0725665c87be98fd5faa9e0
hvanwyk <hanswernervanwyk@gmail.com>
2018-07-22 16:54:28 -0500

    added kernel and form objects

---
+++ tests/test_fem/test_form.py
@@ -0,0 +1,19 @@
+import unittest
+from mesh import QuadMesh, Mesh1D
+from fem import QuadFE, System, Function, GaussRule, Form
+import numpy as np
+
+class TestForm(unittest.TestCase):
+    """
+    Test Form 
+    """
+    def test_constructor(self):
+        pass
+    
+    def test_derivatives(self):
+        pass
+    
+    def test_eval(self):
+        pass
+    
+    
